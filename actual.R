14f072dcd7866b5231f756d238c03cfea74d63c0
Shruti Piduri <s.piduri@gmail.com>
2022-02-03 14:29:04 -0800

    finished course success table

--- actual.R
+++ actual.R
@@ -2,71 +2,82 @@
 #try 
 
 #################
-filter_ay <- 2014
+filter_ay <- 2017
 filter_disagg <- "Overall"
 goal_col <- "2015"
 #################
 
-
 #import packages, read in full csv file
-library(data.table); library(tidyverse)
+library(data.table); library(tidyverse); library(zoo); library(scales)
+library(janitor)
 full <- fread("ssm_sjcc.csv", header = "auto")
-
-
-allyear <- unique(full$academicYear)
-allyears <- vector()
-for(i in 1:6) {
-  allyears[i] = toString(allyear[i])
-}
 
 #alternative
 overall <- full %>% 
-  filter(disagg == filter_disagg, academicYear > filter_ay) %>% 
+  filter(disagg == filter_disagg) %>% 
   select(academicYear, metricID, categoryID, value, perc)
 
 #create row for actual course success metrics
 cslong <- overall %>% 
   filter(metricID == "SM 408SW") %>% 
   select(academicYear, perc) 
-cswide <- cslong %>% 
-  pivot_wider(names_from=academicYear, values_from = perc)
 
-temp <- cslong %>% 
-  mutate(yr1_before = lag(perc, n = 1),
-         yr2_before = lag(perc, n = 2),
-         yr3_avg = (perc + yr1_before + yr2_before) / 3)
+#get the floor goals
+floor <- rollmean(cslong$perc, 3, align = "right")*.9
+floor <- c(rep(0,2), floor)
+cslong <- cslong %>% 
+  add_column(floor)
 
+#get the aspirational goals
+#find the actual percentage corresponding to the first year being looked at
+#take the whole row
+cs_ay <- cslong %>% 
+  filter(academicYear == filter_ay)
+#grab the percentage
+cs_base <- round(cs_ay$perc, digits = 2)
+#set the increment to increase asp goal by each year
+inc = .005
+
+#filter to just the years being looked at
+cslong <- cslong %>% 
+  filter(academicYear >= filter_ay)
+
+#get the number of years being looked at in total (from set starting year to end of data)
+csceil <- cs_base+(inc*(length(cslong$academicYear)-1))
+
+#create vector with aspirational goals
+aspirational = seq(cs_base, csceil, by=inc)
+
+#add aspirational goal to table
+cslong <- cslong %>% add_column(aspirational)
+
+##transpose table and add academic years as column names
+cslong1 <- cslong %>% 
+  column_to_rownames("academicYear")
+
+cswide <- cslong1 %>% 
+  t %>% 
+  as.data.frame()
+
+################################
 
 #vector of relevant category ids (603 = cert, 631 = degree, 608 = adt)
 catids <- c(603, 631, 608, 614)
 
-#table for degree and cert earners
-grads <- overall %>% 
+#data for degree and cert earners
+gradslong <- overall %>% 
     filter(categoryID %in% catids) %>% 
-    select(academicYear, categoryID, value) %>% 
+    select(categoryID, academicYear, value) %>% 
+    group_by(categoryID) %>% 
+    arrange(categoryID)
+
+group_keys(gradslong)
+
+gradlist <- group_split(gradslong)
+
+#take data out of list format and just retain tibble
+gradlist[1]
+
+
+gradswide <- gradslong %>% 
     pivot_wider(names_from=academicYear, values_from = value)
-
-#add in rows in coursesuccess for aspirational and floor goals
-cswide <- add_column(cswide, Goal = "Actual", .before = allyears[1])
-
-seq_along(cswide)
-cswide3 <- vector("double", ncol(cswide))
-
-for(i in 4:length(cswide)){
-  cswide2[[i]] <- mean(cswide[[i]], cswide[[i-1]], cswide[[i-2]])*.9
-}
-
-#creates a set of empty vectors to hold floor values
-for(j in seq_along(catids)){
-  assign(paste0("grads", catids[j], sep=""), vector("integer", ncol(grads)))
-}
-
-floors <- function(x, y) {
-  for(i in 4:length(x)){
-    y[[i]] <- mean(x[[i]], x[[i-1]], x[[i-2]])*.9
-  }
-}
-
-floors(cswide, cswide)
-
-rbind(cswide, as.list(cswide2)) 
