2534a0778e12f7de0701cd725f5948441b261ce1
Shruti Piduri <s.piduri@gmail.com>
2022-02-04 17:34:25 -0800

    added clarification comments

--- gender.R
+++ gender.R
@@ -1,14 +1,16 @@
-#creates a table of course success disaggregated by gender
+##creates five tables of metrics disaggregated by gender
 
 #vector of relevant category ids (603 = cert, 631 = degree, 608 = adt)
 catids <- c(603, 631, 608, 614)
-
 filter_ay <- 2016
+
+#read table
 library(data.table); library(tidyverse); library(zoo); library(scales)
 full <- fread("ssm_sjcc.csv", header = "auto")
+#delete n/as in categoryID
 full$categoryID <- replace_na(full$categoryID, "")
 
-#alternative
+#create gender disaggregated dataset
 gender <- full %>% 
   mutate(years=paste(as.character(academicYear-1), as.character(academicYear), sep="-")) %>% 
   mutate(ids=paste(metricID, categoryID, sep=" ")) %>% 
@@ -20,10 +22,13 @@ csgender <- gender %>%
   filter(metricID == "SM 408SW") %>% 
   select(years, ids, title, description, subgroup, perc)
 
+#graduation/certificates by gender
 ggrads <- gender %>% 
   filter(categoryID %in% catids) %>% 
   select(years, ids, title, description, categoryID, subgroup, value)
 
+#put each metric into a separate tibble
+#each metric is now disaggregated by gender
 ggrads603 <- ggrads %>% 
   filter(categoryID == 603)
 ggrads614 <- ggrads %>% 
--- race.R
+++ race.R
@@ -1,25 +1,30 @@
+##creates five tables of metrics disaggregated by ethnicity
+
 filter_ay <- 2016
 library(data.table); library(tidyverse); library(zoo); library(scales)
 full <- fread("ssm_sjcc.csv", header = "auto")
 full$categoryID <- replace_na(full$categoryID, "")
 
-#alternative
+#create ethnicity disaggregated dataset
 race <- full %>% 
   mutate(years=paste(as.character(academicYear-1), as.character(academicYear), sep="-")) %>% 
   mutate(ids=paste(metricID, categoryID, sep=" ")) %>% 
   filter(disagg == "Ethnicity", academicYear > filter_ay) %>% 
   select(academicYear, years, ids, title, description, metricID, categoryID, subgroup, value, perc)
 
+#create course success table disagg by ethnicity
 csrace <- race %>% 
   filter(metricID == "SM 408SW") %>% 
   select(years, ids, title, description, subgroup, perc)
 
 catids <- c(603, 631, 608, 614)
 
-#table for degree and cert earners
+#table for grad/certs 
 rgrads <- race %>% 
   filter(categoryID %in% catids) %>% 
   select(years, ids, title, description, categoryID, subgroup, value)
+
+#disaggregation by ethnicity for each metric
 rgrads603 <- rgrads %>% 
   filter(categoryID == 603)
 rgrads614 <- rgrads %>% 
