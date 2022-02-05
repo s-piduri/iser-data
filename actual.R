993cfb51aaeacafbdfe0252b692197c7f2d86b08
Shruti Piduri <s.piduri@gmail.com>
2022-02-04 16:35:19 -0800

    edited long tables

--- actual.R
+++ actual.R
@@ -5,26 +5,28 @@
 filter_ay <- 2017
 filter_csu <- 2016
 filter_disagg <- "Overall"
-goal_col <- "2015"
 #################
 
 #import packages, read in full csv file
-#if (!require("s2")) {install.packages("s2"); require("s2")}
+#if (!require("zoo")) {install.packages("zoo"); require("zoo")}
 
 library(data.table); library(tidyverse); library(zoo); library(scales)
-library(janitor)
 full <- fread("ssm_sjcc.csv", header = "auto")
+full$categoryID <- replace_na(full$categoryID, "")
+full$metricID
 
 #alternative
 overall <- full %>% 
   filter(disagg == filter_disagg) %>% 
-  select(academicYear, metricID, categoryID, value, perc)
+  mutate(years=paste(as.character(academicYear-1), as.character(academicYear), sep="-")) %>% 
+  mutate(ids=paste(metricID, categoryID, sep=" ")) %>% 
+  select(academicYear, years, ids, title, description, metricID, categoryID, value, perc)
+
 
 ###########course success table#########
 #create row for actual course success metrics
 cslong <- overall %>% 
-  filter(metricID == "SM 408SW") %>% 
-  select(academicYear, perc) 
+  filter(ids == "SM 408SW ")
 
 #get the floor goals
 floor <- rollmean(cslong$perc, 3, align = "right")*.9
@@ -53,16 +55,18 @@ csceil <- cs_base+(inc*(length(cslong$academicYear)-1))
 aspirational = seq(cs_base, csceil, by=inc)
 
 #add aspirational goal to table
-cslong <- cslong %>% add_column(aspirational, .before="perc")
+cslong <- cslong %>% 
+  add_column(aspirational, .before="perc") %>% 
+  select(years, ids, title, description, floor, aspirational, perc)
 
 ##transpose table and add academic years as column names
-cslong1 <- cslong %>% 
-  column_to_rownames("academicYear")
-
-cswide <- cslong1 %>% 
-  t %>% 
-  as.data.frame() %>% 
-  rownames_to_column(" ")
+# cslong1 <- cslong %>% 
+#   column_to_rownames("academicYear")
+# 
+# cswide <- cslong1 %>% 
+#   t %>% 
+#   as.data.frame() %>% 
+#   rownames_to_column(" ")
 
 #########grad/cert/degree tables###############
 
@@ -71,12 +75,10 @@ catids <- c(603, 631, 608, 614)
 
 #data for degree and cert earners
 gradslong <- overall %>% 
-    filter(categoryID %in% catids) %>% 
-    select(categoryID, academicYear, value) %>% 
-    group_by(categoryID) %>% 
-    arrange(categoryID)
-
-group_keys(gradslong)
+  filter(categoryID %in% catids) %>% 
+  select(academicYear, years, ids, title, description, metricID, categoryID, value, perc)
+  #group_by(categoryID) %>% 
+  #arrange(categoryID)
 
 #splits grads dataset by categoryID
 grads603 <- gradslong %>% 
@@ -90,10 +92,10 @@ grads631 <- gradslong %>%
 
 ####### grads603 = certificate earners = floor and aspirational data#######
 
-floor603 <- floor(rollmean(grads603$value, 3, align = "right")*.9)
-floor603 <- c(rep(0,2), floor603)
+floor <- floor(rollmean(grads603$value, 3, align = "right")*.9)
+floor <- c(rep(0,2), floor)
 grads603 <- grads603 %>% 
-  add_column(floor603, .before="value")
+  add_column(floor, .before="value")
 
 #get the aspirational goals
 #find the actual percentage corresponding to the first year being looked at
@@ -118,30 +120,33 @@ yrlngth = length(grads603$academicYear)-1
 cert_incr <- (aspir/cert_base)^(1/5)
 
 #create vector with aspirational goals
-aspir603 = round(cumprod(c(cert_base, rep(cert_incr,yrlngth))))
+aspirational = round(cumprod(c(cert_base, rep(cert_incr,yrlngth))))
 
 #add aspirational goal to table
-grads603 <- grads603 %>% add_column(aspir603, .before="value")
+grads603 <- grads603 %>% 
+  add_column(aspirational, .before="value")
+grads603 <- grads603 %>% 
+  select(years, ids, title, description, floor, aspirational, value)
 
 ##add academic years as column names
-grads6031 <- grads603 %>% 
-  column_to_rownames("academicYear")
-
-#transpose table
-grads603wide <- grads6031 %>% 
-  t %>% 
-  as.data.frame() %>% 
-  rownames_to_column(" ")
+# grads6031 <- grads603 %>% 
+#   column_to_rownames("academicYear")
+# 
+# #transpose table
+# grads603wide <- grads6031 %>% 
+#   t %>% 
+#   as.data.frame() %>% 
+#   rownames_to_column(" ")
 
 #drop categoryID row
-grads603wide <- grads603wide[!(row.names(grads603wide) %in% c('categoryID')), ]
+#grads603wide <- grads603wide[!(row.names(grads603wide) %in% c('categoryID')), ]
 
 ####### grads631 = degree earners = floor and aspirational data#######
 
-floor631 <- floor(rollmean(grads631$value, 3, align = "right")*.9)
-floor631 <- c(rep(0,2), floor631)
+floor <- floor(rollmean(grads631$value, 3, align = "right")*.9)
+floor <- c(rep(0,2), floor)
 grads631 <- grads631 %>% 
-  add_column(floor631, .before="value")
+  add_column(floor, .before="value")
 
 #get the aspirational goals
 #find the actual percentage corresponding to the first year being looked at
@@ -163,29 +168,31 @@ yrlngth = length(grads631$academicYear)-1
 deg_incr <- (aspir/deg_base)^(1/5)
 
 #create vector with aspirational goals
-aspir631 = round(cumprod(c(deg_base, rep(deg_incr,yrlngth))))
+aspirational = round(cumprod(c(deg_base, rep(deg_incr,yrlngth))))
 
 #add aspirational goal to table
-grads631 <- grads631 %>% add_column(aspir631, .before="value")
+grads631 <- grads631 %>% 
+  add_column(aspirational, .before="value") %>% 
+  select(years, ids, title, description, floor, aspirational, value)
 
-##transpose table and add academic years as column names
-grads6311 <- grads631 %>% 
-  column_to_rownames("academicYear")
-
-grads631wide <- grads6311 %>% 
-  t %>% 
-  as.data.frame() %>% 
-  rownames_to_column(" ")
-
-grads631wide <- grads631wide[!(row.names(grads631wide) %in% c('categoryID')), ]
+# ##transpose table and add academic years as column names
+# grads6311 <- grads631 %>% 
+#   column_to_rownames("academicYear")
+# 
+# grads631wide <- grads6311 %>% 
+#   t %>% 
+#   as.data.frame() %>% 
+#   rownames_to_column(" ")
+# 
+# grads631wide <- grads631wide[!(row.names(grads631wide) %in% c('categoryID')), ]
 
 
 ####### grads608 = adt earners = floor and aspirational data#####
 
-floor608 <- round(rollmean(grads608$value, 3, align = "right")*.9)
-floor608 <- c(rep(0,2), floor608)
+floor <- round(rollmean(grads608$value, 3, align = "right")*.9)
+floor <- c(rep(0,2), floor)
 grads608 <- grads608 %>% 
-  add_column(floor608, .before="value")
+  add_column(floor, .before="value")
 
 #get the aspirational goals
 #find the actual percentage corresponding to the first year being looked at
@@ -207,29 +214,31 @@ yrlngth = length(grads608$academicYear)-1
 adt_incr <- (aspir/adt_base)^(1/5)
 
 #create vector with aspirational goals
-aspir608 = round(cumprod(c(adt_base, rep(adt_incr,yrlngth))))
+aspirational = round(cumprod(c(adt_base, rep(adt_incr,yrlngth))))
 
 #add aspirational goal to table
-grads608 <- grads608 %>% add_column(aspir608, .before="value")
+grads608 <- grads608 %>% 
+  add_column(aspirational, .before="value") %>% 
+  select(years, ids, title, description, floor, aspirational, value)
 
-##transpose table and add academic years as column names
-grads6081 <- grads608 %>% 
-  column_to_rownames("academicYear")
-
-grads608wide <- grads6081 %>% 
-  t %>%   
-  as.data.frame() %>% 
-  rownames_to_column(" ")
-
-grads608wide <- grads608wide[!(row.names(grads608wide) %in% c('categoryID')), ]
+# ##transpose table and add academic years as column names
+# grads6081 <- grads608 %>% 
+#   column_to_rownames("academicYear")
+# 
+# grads608wide <- grads6081 %>% 
+#   t %>%   
+#   as.data.frame() %>% 
+#   rownames_to_column(" ")
+# 
+# grads608wide <- grads608wide[!(row.names(grads608wide) %in% c('categoryID')), ]
 
 
 ####### grads614 = distinct uc/csu transfer = floor and aspirational data#######
 
-floor614 <- floor(rollmean(grads614$value, 3, align = "right")*.9)
-floor614 <- c(0, grads614$value[2], floor614)
+floor <- floor(rollmean(grads614$value, 3, align = "right")*.9)
+floor <- c(0, grads614$value[2], floor)
 grads614 <- grads614 %>% 
-  add_column(floor614, .before="value")
+  add_column(floor, .before="value")
 
 #get the aspirational goals
 #find the actual percentage corresponding to the first year being looked at
@@ -251,22 +260,26 @@ yrlngth = length(grads614$academicYear)-1
 csu_incr <- (aspir/csu_base)^(1/5)
 
 #create vector with aspirational goals
-aspir614 = round(cumprod(c(csu_base, rep(csu_incr,yrlngth))))
+aspirational = round(cumprod(c(csu_base, rep(csu_incr,yrlngth))))
 
 #add aspirational goal to table
-grads614 <- grads614 %>% add_column(aspir614, .before="value")
+grads614 <- grads614 %>% 
+  add_column(aspirational, .before="value") %>% 
+  select(years, ids, title, description, floor, aspirational, value)
 
-##transpose table and add academic years as column names
-grads6141 <- grads614 %>% 
-  column_to_rownames("academicYear")
+# ##transpose table and add academic years as column names
+# grads6141 <- grads614 %>% 
+#   column_to_rownames("academicYear")
+# 
+# grads614wide <- grads6141 %>% 
+#   t %>%   
+#   as.data.frame() %>% 
+#   rownames_to_column(" ")
+# 
+# grads614wide <- grads614wide[!(row.names(grads614wide) %in% c('categoryID')), ]
+# 
 
-grads614wide <- grads6141 %>% 
-  t %>%   
-  as.data.frame() %>% 
-  rownames_to_column(" ")
-
-grads614wide <- grads614wide[!(row.names(grads614wide) %in% c('categoryID')), ]
-
-
-
+############
 save(cswide, grads603wide, grads608wide, grads614wide, grads631wide, file = "finaltbl.RData")
+
+save(cslong, grads603, grads608, grads614, grads631, file = "finaltbl.RData")
