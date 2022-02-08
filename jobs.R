d0d9d676cb61e80033494b9e20b3025c7741400b
Shruti Piduri <s.piduri@gmail.com>
2022-02-08 10:27:04 -0800

    added jobs.r to read job and licensing data

---
+++ jobs.R
@@ -0,0 +1,9 @@
+library(tidyverse);
+
+path <- "C:\\Users\\spiduri\\San Jose-Evergreen Community College District\\SJCC RPIE - Documents\\ACCJC Annual Reports\\CTE Licence and Job Placement Data\\Licensing Exam Pass Rates For Ann Rpt2022.xlsx"
+licensure <- read_excel(path=path, sheet = 1)
+jobplace <- read_excel(path=path, sheet = 2)
+
+#drop the rows that don't contain any data
+licensure <- drop_na(licensure, `FY18/19`)
+jobplace <- drop_na(jobplace, `FY18/19`)
