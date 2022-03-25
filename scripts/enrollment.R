77481d17e42ff78259cc23f0bbd39ef5520dae48
Shruti Piduri <s.piduri@gmail.com>
2022-03-25 16:17:29 -0700

    comments

--- scripts/enrollment.R
+++ scripts/enrollment.R
@@ -15,11 +15,48 @@ sp_hc <- sp_hc %>%
   replace(is.na(.), 0)
 
 
-total_hc <- enrlmnt_raw %>% 
+fa2018 <- enrlmnt_raw %>% 
+  filter(term_id == '2022SP')
+
+length(unique(fa2018$student_id))
+  
+#headcount by term
+term_hc <- enrlmnt_raw %>% 
+  mutate(term_cid = case_when(
+    grepl("SPI", term_id,fixed=TRUE) ~ substr(term_id,1,nchar(term_id)-1),
+    TRUE ~ as.character(term_id))) %>% 
+  mutate(student_yr = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_yr) %>% 
+  arrange(student_yr, term_reporting_year) %>% 
+  mutate(acad_year_unique = row_number()) %>%
+  filter(acad_year_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id, term_reporting_year) %>% 
+  summarize(headcount = n())
+
+#headcount by year
+annual_hc <- enrlmnt_raw %>% 
+  mutate(student_yr = paste(student_id, term_reporting_year, sep="_")) %>% 
+  group_by(student_yr) %>% 
+  arrange(student_yr, term_reporting_year) %>% 
+  mutate(acad_year_unique = row_number()) %>%
+  filter(acad_year_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_reporting_year) %>% 
+  summarize(headcount = n())
+
+#specifically fall headcounts
+fall_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
   mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
   group_by(student_term) %>% 
   arrange(student_term, term_reporting_year) %>% 
-  mutate(row_n = row_number())
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id) %>% 
+  summarize(headcount = n())
+
 
 
 save(sp_hc, file="entable.RData")
