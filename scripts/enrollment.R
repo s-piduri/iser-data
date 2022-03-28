a311aeb7b56e905af4bd0e2ebf1459a62b2f2179
Shruti Piduri <s.piduri@gmail.com>
2022-03-28 15:23:28 -0700

    headcount/seatcount tables

--- scripts/enrollment.R
+++ scripts/enrollment.R
@@ -1,7 +1,4 @@
-
-
 library(tidyverse); library(readxl)
-
 
 #head count by special populations
 sp <- "C:\\Users\\spiduri\\Downloads\\SPStudentCountSumm.csv"
@@ -14,12 +11,76 @@ colnames(sp_hc) <- c("Special Characteristic", "2017", "2018", "2019",
 sp_hc <- sp_hc %>% 
   replace(is.na(.), 0)
 
+# length(unique(fa2018$student_id))
 
-fa2018 <- enrlmnt_raw %>% 
-  filter(term_id == '2022SP')
+####overall headcounts#####
+#headcount by year
+annual_hc <- enrlmnt_raw %>% 
+  mutate(student_yr = paste(student_id, term_reporting_year, sep="_")) %>% 
+  mutate(annual = paste(term_reporting_year, (term_reporting_year+1), sep="-")) %>% 
+  group_by(student_yr) %>% 
+  arrange(student_yr, annual) %>% 
+  mutate(acad_year_unique = row_number()) %>%
+  filter(acad_year_unique == 1) %>%
+  ungroup() %>% 
+  group_by(annual) %>% 
+  summarize(headcount = n()) 
+colnames(annual_hc) <- c("Academic Year", "Headcount")
 
-length(unique(fa2018$student_id))
-  
+#credit headcount by year
+annual_c_hc <- enrlmnt_raw %>% 
+  mutate(student_yr = paste(student_id, term_reporting_year, sep="_")) %>% 
+  mutate(annual = paste(term_reporting_year, (term_reporting_year+1), sep="-")) %>%
+  filter(!(grepl("-5", crs_name,fixed=TRUE))) %>% 
+  group_by(student_yr) %>% 
+  arrange(student_yr, annual) %>% 
+  mutate(acad_year_unique = row_number()) %>%
+  filter(acad_year_unique == 1) %>%
+  ungroup() %>% 
+  group_by(annual) %>% 
+  summarize(headcount = n()) 
+colnames(annual_c_hc) <- c("Academic Year", "Headcount")
+
+#noncredit headcount by year
+annual_nc_hc <- enrlmnt_raw %>% 
+  mutate(student_yr = paste(student_id, term_reporting_year, sep="_")) %>% 
+  mutate(annual = paste(term_reporting_year, (term_reporting_year+1), sep="-")) %>%
+  filter((grepl("-5", crs_name,fixed=TRUE))) %>% 
+  group_by(student_yr) %>% 
+  arrange(student_yr, annual) %>% 
+  mutate(acad_year_unique = row_number()) %>%
+  filter(acad_year_unique == 1) %>%
+  ungroup() %>% 
+  group_by(annual) %>% 
+  summarize(headcount = n()) 
+colnames(annual_nc_hc) <- c("Academic Year", "Headcount")
+
+#####overall seatcounts#####
+#overall seatcount by year
+annual_sc <- enrlmnt_raw %>% 
+  mutate(annual = paste(term_reporting_year, (term_reporting_year+1), sep="-")) %>%
+  group_by(annual) %>% 
+  summarize(seatcount = n())
+colnames(annual_sc) <- c("Academic Year", "Seatcount")
+
+#overall seatcount by year
+annual_c_sc <- enrlmnt_raw %>% 
+  mutate(annual = paste(term_reporting_year, (term_reporting_year+1), sep="-")) %>%
+  filter(!(grepl("-5", crs_name,fixed=TRUE))) %>% 
+  group_by(annual) %>% 
+  summarize(seatcount = n())
+colnames(annual_c_sc) <- c("Academic Year", "Seatcount")
+
+#overall seatcount by year
+annual_nc_sc <- enrlmnt_raw %>% 
+  mutate(annual = paste(term_reporting_year, (term_reporting_year+1), sep="-")) %>%
+  filter(grepl("-5", crs_name,fixed=TRUE)) %>% 
+  group_by(annual) %>% 
+  summarize(seatcount = n())
+colnames(annual_nc_sc) <- c("Academic Year", "Seatcount")
+
+
+#####headcount by term overall#####
 #headcount by term
 term_hc <- enrlmnt_raw %>% 
   mutate(term_cid = case_when(
@@ -33,17 +94,7 @@ term_hc <- enrlmnt_raw %>%
   ungroup() %>% 
   group_by(term_id, term_reporting_year) %>% 
   summarize(headcount = n())
-
-#headcount by year
-annual_hc <- enrlmnt_raw %>% 
-  mutate(student_yr = paste(student_id, term_reporting_year, sep="_")) %>% 
-  group_by(student_yr) %>% 
-  arrange(student_yr, term_reporting_year) %>% 
-  mutate(acad_year_unique = row_number()) %>%
-  filter(acad_year_unique == 1) %>%
-  ungroup() %>% 
-  group_by(term_reporting_year) %>% 
-  summarize(headcount = n())
+colnames(term_hc) <- c("Term", "Headcount")
 
 #specifically fall headcounts
 fall_hc <- enrlmnt_raw %>% 
@@ -56,8 +107,94 @@ fall_hc <- enrlmnt_raw %>%
   ungroup() %>% 
   group_by(term_id) %>% 
   summarize(headcount = n())
+colnames(fall_hc) <- c("Fall Term", "Headcount")
+
+#####fall credit headcounts#####
+#fall credit headcounts disaggregation
+fall_cred_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
+  filter(!(grepl("-5", crs_name,fixed=TRUE))) %>% 
+  #filter(!(cred_type_desc== "Noncredit Course")) %>% 
+  mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_term) %>% 
+  arrange(student_term, term_reporting_year) %>% 
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id) %>% 
+  summarize(headcount = n())
+colnames(fall_cred_hc) <- c("Fall Term", "Headcount")
+
+#credit headcounts by student type
+fall_type_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
+  filter(!grepl("-5", crs_name,fixed=TRUE)) %>% 
+  mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_term) %>% 
+  arrange(student_term, term_reporting_year) %>% 
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id, sb15_term_status) %>% 
+  summarize(headcount = n())
+colnames(fall_type_hc) <- c("Fall Term", "Student Type", "Headcount")
+
+#credit headcounts by goal
+fall_goal_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
+  filter(!grepl("-5", crs_name,fixed=TRUE)) %>% 
+  mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_term) %>% 
+  arrange(student_term, term_reporting_year) %>% 
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id, educ_goal_desc) %>% 
+  summarize(headcount = n())
+colnames(fall_goal_hc) <- c("Fall Term", "Educational Goal", "Headcount")
+
+#credit headcounts by age
+fall_age_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
+  filter(!grepl("-5", crs_name,fixed=TRUE)) %>% 
+  mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_term) %>% 
+  arrange(student_term, term_reporting_year) %>% 
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id, age) %>% 
+  summarize(headcount = n())
+colnames(fall_age_hc) <- c("Fall Term", "Age Group", "Headcount")
+
+#credit headcounts by race
+fall_race_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
+  filter(!grepl("-5", crs_name,fixed=TRUE)) %>% 
+  mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_term) %>% 
+  arrange(student_term, term_reporting_year) %>% 
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id, race) %>% 
+  summarize(headcount = n())
+colnames(fall_race_hc) <- c("Fall Term", "Race", "Headcount")
+
+#fall credit headcounts by gender
+fall_gender_hc <- enrlmnt_raw %>% 
+  filter(grepl("FA", term_id)) %>% 
+  filter(!grepl("-5", crs_name,fixed=TRUE)) %>% 
+  mutate(student_term = paste(student_id, term_id, sep="_")) %>% 
+  group_by(student_term) %>% 
+  arrange(student_term, term_reporting_year) %>% 
+  mutate(fall_unique = row_number()) %>% 
+  filter(fall_unique == 1) %>%
+  ungroup() %>% 
+  group_by(term_id, gender) %>% 
+  summarize(headcount = n())
+colnames(fall_gender_hc) <- c("Fall Term", "Gender", "Headcount")
 
 
-
-save(sp_hc, file="entable.RData")
-
+##save####
+save(sp_hc, term_hc, annual_hc, fall_hc, file="entable.RData")
